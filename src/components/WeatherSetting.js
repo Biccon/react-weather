102c98ea7abce13079d399ea5f8f0851c73e2d7d
Biccon <admin@idx.kr>
2018-09-29 20:51:57 +0900

    redux, react-redux 도입 준비

--- src/components/WeatherSetting.js
+++ src/components/WeatherSetting.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import "./WeatherSetting.scss";
 
 class WeatherSetting extends Component {
+  componentDidMount() { 
+;  }
   render() {
     const { state } = this.props;
-    return <div>Setting weather {state.city}
-      <button>도시</button> 
-    </div>;
+    return (
+      <div>
+        Setting weather {state.city}
+        <button>도시</button>
+      </div>
+    );
   }
 }
 export default WeatherSetting;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { CHANGE_CITY } from "../actions";
+import { combineReducers } from "redux";
+
+const weatherInitialState = {
+  city: "Seoul, KR"
+};
