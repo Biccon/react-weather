7c23e49d073ffd55c7a96c8938764b3f8c9b8559
biccon <admin@idx.kr>
2018-09-30 20:53:48 +0900

    forecast changing

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,55 +1,77 @@
 import React, { Component } from "react";
-import "./WeatherForecast.scss"; 
+import "./WeatherForecast.scss";
 import Loading from "./Loading";
+import axios from "axios";
 
 class WeatherForecast extends Component {
   state = {
-    loading: false
+    loading: false,
+    weather: null
   };
-  /*
 
   componentDidMount() {
-    console.log("처음 마운트 됐을 때 날씨 구하기", this.state);
-    this.getForecast();
+    this.getForecast(this.props.city.id);
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.state.city.id !== nextProps.city.id) {
-      console.log("도시 다른거 선택할 때 날씨 구하기", nextProps);
-      this.getForecast();
+    if (this.props.city.id !== nextProps.city.id) {
+      this.getForecast(nextProps.city.id);
     }
   }
-
-  getForecast = () => {
-    const id = this.state.city.id;
+  round = (floatNumber, number) => {
+    return Math.round(floatNumber * 10 ** number) / 10 ** number;
+  };
+  getForecast = id => {
+    console.log("getForecastWeather_city id : ", id);
     this.setState({
       loading: true
     });
     axios
       .get(
-        `http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=27a2181466830267d7ac26415db7e609`
+        `http://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=27a2181466830267d7ac26415db7e609&units=metric`
       )
-      .then(e => {
-        console.log(e);
+      .then(response => {
+        const data = response.data;
+        const main = data["main"];
+        const clouds = data["clouds"];
+        const weather = data["weather"];
+        const wind = data["wind"];
+        const sys = data["sys"];
+
+        const newData = {
+          name: data["name"],
+          humidity: main.humidity,
+          pressure: main.pressure,
+          temp: {
+            temp: this.round(main.temp, 1),
+            temp_max: this.round(main.temp_max, 1),
+            temp_min: this.round(main.temp_min, 1)
+          },
+          clouds: clouds.all,
+          wind: {
+            speed: this.round(wind.speed, 1),
+            deg: this.round(wind.deg, 0)
+          },
+          weather: weather,
+          sun: { sunrise: sys.sunrise, sunset: sys.sunset }
+        };
         this.setState({
-          loading: false
-        })
+          loading: false,
+          weather: newData
+        });
       })
-      .catch(e => {
+      .catch(error => {
+        console.log("error", error);
         this.setState({
           loading: false
-        })
+        });
       });
   };
 
-  constructor(props) {
-    super(props);
-    console.log("컨스트럭터 작동, props is : ", props);
-    this.state = {
-      city: props.city
-    };
-  }
-  */
+  refresh = () => {
+    this.getForecast(this.props.city.id);
+  };
+
   render() {
     const { city } = this.props;
     return (
