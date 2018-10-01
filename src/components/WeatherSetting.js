831b8b5375e95213c6759140ad7670935f5fc726
biccon <admin@idx.kr>
2018-10-01 16:40:45 +0900

    select 제거

--- src/components/WeatherSetting.js
+++ src/components/WeatherSetting.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./WeatherSetting.scss";
-import DropdownSelect from "./DropdownSelect";
 
 class WeatherSetting extends Component {
   componentDidMount() {}
@@ -13,20 +12,18 @@ class WeatherSetting extends Component {
   render() {
     const { cities, currentIndex, onCityChange } = this.props;
     const cityChange = e => {
-      alert('a');
-      console.log(e);
       const changedIndex = e.target.value;
-      console.log(changedIndex);
       onCityChange(changedIndex);
     };
     return (
       <div className="weather-setting">
-        <DropdownSelect
-          name="city"
-          handleChange={cityChange}
-          value={currentIndex}
-          list={cities.map(city => city.name)}
-        />
+        <select id="city" onChange={cityChange} value={currentIndex}>
+          {cities.map((city, i) => (
+            <option value={i} key={i}>
+              {city.name}
+            </option>
+          ))}
+        </select>
       </div>
     );
   }
